907e770e77d728043abb55a5865e8fca9bd8ad80
JonL <lai.c.jonathan@gmail.com>
2023-05-17 12:57:09 +0700

    branch update alpha

--- src/components/ProfileCard.vue
+++ src/components/ProfileCard.vue
@@ -1,21 +1,17 @@
 <script setup>
-// Passed-through data
 defineProps({
   data: {}
 })
 
-// Local Data
-const message = "Hello" 
-
+const message = "Hello"
 </script>
 
 <template>
-  <div class="flex flex-col items-center gap-x-6">
-    <p>{{ message }}</p>
+  <div class="flex flex-row items-center gap-x-6">
     <img class="h-32 w-32 rounded-full" :src="data.imageUrl" alt="" />
-    <div class="mx-auto">
-      <h3 class="text-base font-semibold tracking-tight text-gray-900">{{ data.name }}</h3>
-      <p class="text-sm font-semibold text-indigo-600">{{ data.role }}</p>
+    <div>
+      <h3 class="text-base font-semibold leading-7 tracking-tight text-gray-900">{{ data.name }}</h3>
+      <p class="text-sm font-semibold leading-6 text-indigo-600">{{ data.role }}</p>
     </div>
   </div>
 </template>
